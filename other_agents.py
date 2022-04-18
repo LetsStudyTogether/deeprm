c20ff8ff04984840f173f6c10bad1916ccad8a60
Hyejin Kim <hyejink0212@gmail.com>
2022-04-18 11:21:58 -0400

    data locality calculation merged

--- other_agents.py
+++ other_agents.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import itertools
 
 def get_packer_action(machines, job_slot):
         align_score = 0
@@ -13,8 +13,30 @@ def get_packer_action(machines, job_slot):
                 if can_allocate: # enough resource to allocate
                     idx_of_res_alloc = [[] for i in range(machines.num_machines)] ##list of resource indices to be allocated per machine
 
-                    for res_idx, machine_idxs in enumerate(idx_of_abvl_res):
-                        idx_of_res_alloc[machine_idxs[0]].append(res_idx) #take the first index for now
+                    threshold_score = (machines.num_machines - 1) ** 2 * machines.num_res / 4
+
+                    current_minimum_score = 0.0
+                    current_minimum_combination = []
+
+                    all_combinations = itertools.product(*idx_of_abvl_res)
+                    for tmp in all_combinations:
+                        tmp_score = 0.0
+                        for machine_idx in range(1,len(tmp)):
+                            tmp_score += (tmp[machine_idx] - tmp[0]) ** 2
+                        # print tmp, tmp_score
+                        # current_minimum_combination = list(tmp)
+                        if tmp_score <= threshold_score:
+                            current_minimum_combination = list(tmp)
+                            break
+                        else:
+                            if tmp_score <= current_minimum_score:
+                                current_minimum_combination = list(tmp)
+                                current_minimum_score = tmp_score
+
+                    # print current_minimum_combination
+
+                    for res_idx, machine_idx in enumerate(current_minimum_combination):
+                        idx_of_res_alloc[machine_idx].append(res_idx)
 
                     tmp_align_score = 0
                     for machine_idx, res_alloc_list in enumerate(idx_of_res_alloc):
@@ -62,8 +84,30 @@ def get_packer_sjf_action(machines, job_slot, knob):  # knob controls which to f
                 if can_allocate: # enough resource to allocate
                     idx_of_res_alloc = [[] for i in range(machines.num_machines)] ##list of resource indices to be allocated per machine
 
-                    for res_idx, machine_idxs in enumerate(idx_of_abvl_res):
-                        idx_of_res_alloc[machine_idxs[0]].append(res_idx) #take the first index for now
+                    threshold_score = (machines.num_machines - 1) ** 2 * machines.num_res / 4
+
+                    current_minimum_score = 0.0
+                    current_minimum_combination = []
+
+                    all_combinations = itertools.product(*idx_of_abvl_res)
+                    for tmp in all_combinations:
+                        tmp_score = 0.0
+                        for machine_idx in range(1,len(tmp)):
+                            tmp_score += (tmp[machine_idx] - tmp[0]) ** 2
+                        # print tmp, tmp_score
+                        # current_minimum_combination = list(tmp)
+                        if tmp_score <= threshold_score:
+                            current_minimum_combination = list(tmp)
+                            break
+                        else:
+                            if tmp_score <= current_minimum_score:
+                                current_minimum_combination = list(tmp)
+                                current_minimum_score = tmp_score
+
+                    # print current_minimum_combination
+
+                    for res_idx, machine_idx in enumerate(current_minimum_combination):
+                        idx_of_res_alloc[machine_idx].append(res_idx)
 
                     tmp_align_score = 0
                     for machine_idx, res_alloc_list in enumerate(idx_of_res_alloc):
